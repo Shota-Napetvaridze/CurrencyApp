ad97c05db3aa9f66cf10207af1d27f60c3234864
koshokosho <napetvaridze.shota@gmail.com>
2021-05-25 21:53:18 +0400

    CurrencyApp

---
+++ CurrencyApp/Model/CurrencyModel.swift
@@ -0,0 +1,14 @@
+//
+//  CurrencyModel.swift
+//  CurrencyApp
+//
+//  Created by MacBook Pro on 25.05.21.
+//
+
+import UIKit
+
+struct CurrencyModel {
+    let name: String
+    let amount: String
+    let abbreviation: String
+}
